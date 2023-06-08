82e191e8467b914644f90bb41503d82480383bd8
akif <akifarhan@gmail.com>
2023-06-08 16:16:28 +0800

    Add return type to route

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,19 @@
+import 'package:codegen/models/models.dart';
 import 'package:codegen/ui/bottom_sheets/notice/notice_sheet.dart';
 import 'package:codegen/ui/dialogs/info_alert/info_alert_dialog.dart';
 import 'package:codegen/ui/views/home/home_view.dart';
 import 'package:codegen/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:codegen/ui/views/settings/settings_view.dart';
 // @stacked-import
 
 @StackedApp(
   routes: [
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
-    // @stacked-route
+    MaterialRoute<User?>(page: SettingsView),
+// @stacked-route
   ],
   dependencies: [
     LazySingleton(classType: BottomSheetService),
